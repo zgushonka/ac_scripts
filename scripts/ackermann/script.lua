321a3b8fee9b882acacae78f8c8a10bef39aa660
zgushonka <zgushonka@gmail.com>
2026-02-28 14:29:11 +0100

    clean up

--- scripts/ackermann/script.lua
+++ scripts/ackermann/script.lua
@@ -2,7 +2,7 @@
 -- by: zgshnk -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2026 zgshnk
 
-local scr_ackermann          = require("scr_ackermann")
+local scr_ackermann = require("scr_ackermann")
 
 function script.update(dt)
     scr_ackermann:draw()
